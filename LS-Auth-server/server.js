032b14c88b0c328d8d53b4d0fb060c941771675a
hohguy <dfourney@gmail.com>
2017-06-13 21:34:12 -0400

    Add DB environment variable

--- LS-Auth-server/server.js
+++ LS-Auth-server/server.js
@@ -3,9 +3,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const db = process.env.DB || 'mongodb://localhost/auth';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/auth');
+mongoose.connect(db);
 
 const app = express();
 app.use(bodyParser.json());
